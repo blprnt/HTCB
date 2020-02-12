e02deff2af05e3d655235b323f20dc6b11c9255e
Jer Thorp <blprnt@blprnt.com>
2020-02-12 14:14:28 -0500

    Create LinkList.ml

---
+++ LinkList.ml
@@ -0,0 +1,9 @@
+* Rose of Jericho - Martin Kim Luge - http://www.martinluge.de/work/roseofjericho/
+* OneTrees - Nathalie Jeremijenko - https://www.deeproot.com/blog/blog-entries/onetrees-the-forgotten-tree-art-project
+* Of All the People in All the World - Stan's Cafe - http://www.stanscafe.co.uk/project-of-all-the-people.html
+
+* NYTimes visualizations - https://www.flickr.com/photos/blprnt/3261847828/in/album-72157613381549987/
+* Notabilia - http://notabilia.net/
+* Tidy St. - https://www.theguardian.com/environment/blog/2011/apr/12/energy-use-households-monitor-electricity
+* Nuage Vert - http://hehe.org.free.fr/hehe/texte/nv/
+* Cupcakes index - http://www.annelieberner.com/Cupcakes-Index
